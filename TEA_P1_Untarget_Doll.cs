520443b2b0888ce76601c565a929f2b918315b16
englundmb-jpg <englund.mb@gmail.com>
2026-08-18 17:25:03 -0700

    Create TEA_P1_Untarget_Doll.cs
    Add TEA P1 Untarget Doll

---
+++ TEA_P1_Untarget_Doll.cs
@@ -0,0 +1,39 @@
+using Dalamud.Game.ClientState.Objects.Types;
+using ECommons;
+using ECommons.DalamudServices;
+using ECommons.ExcelServices.TerritoryEnumeration;
+using Splatoon.SplatoonScripting;
+using System.Collections.Generic;
+using System.Linq;
+
+using ECommons.DalamudServices.Legacy;
+
+namespace SplatoonScriptsOfficial.Duties.Shadowbringers.The_Epic_Of_Alexander;
+
+public class TEA_P1_Untarget_Doll : SplatoonScript
+{
+    public override HashSet<uint>? ValidTerritories { get; } =
+        [Raids.The_Epic_of_Alexander_Ultimate];
+
+    public override Metadata Metadata =>
+        new(1, "NightmareXIV");
+
+    public override void OnUpdate()
+    {
+        if(Svc.Targets.Target is IBattleNpc b
+           && b.NameId.EqualsAny<uint>(3759, 9214)
+           && ((float)b.CurrentHp / (float)b.MaxHp) < 0.24f)
+        {
+            Svc.Targets.Target =
+                Svc.Objects
+                    .OfType<IBattleNpc>()
+                    .FirstOrDefault(x =>
+                        x.IsTargetable
+                        && !x.IsDead
+                        && x.NameId.EqualsAny<uint>(
+                            3765,
+                            9211,
+                            9212));
+        }
+    }
+}
